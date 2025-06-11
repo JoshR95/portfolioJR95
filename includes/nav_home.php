e224511069dc755a355a75309975f77d1abca19d
JoshR95 <joshuarickards@hotmail.co.uk>
2025-06-11 13:22:53 +0100

    added the page-buttons to the nav
    added the page button styling to the nav to keep the styling consistent throughout the page

--- includes/nav_home.php
+++ includes/nav_home.php
@@ -18,19 +18,19 @@
 
     <div class="links-container">
       <ul class="pages-links"> 
-        <!--Portfolio is homepage -->
-        <!-- <li class="btn-one"><a href="./pages/about.php"><span>ABOUT ME</span></a></li>     -->
-        <li><a href="#portfolio"><span>MY PORTFOLIO</span></a></li>
-        <li><a href="./pages/codeExamples.php"><span>CODE EXAMPLES</span></a></li>
-        <li><a href="./pages/scsScheme.php"><span>SCS SCHEME</span></a></li>
-        <li><a href="#contact-me"><span>CONTACT ME</span></a></li>
+        <li><a href="../index.php">MAIN PAGE</a></li>    
+        <li><a href="../index.php#portfolio">MY PORTFOLIO</a></li>
+        <li><a href="../pages/codeExamples.php">CODE EXAMPLES</a></li>
+        <li><a href="../pages/scsScheme.php">SCS SCHEME</a></li>
+        <li><a href="../index.php#contact-me">CONTACT ME</a></li>
       </ul>
+      <div class="nav-socials">
+        <a href="https://github.com/JoshR95">
+          <span class="icon-github"></span>
+        </a>
+      </div>
     </div>
-
-    <div class="nav-socials">
-      <a href="https://github.com/JoshR95" target="_blank" rel="noopener noreferrer">
-        <span class="icon-github"></span>
-      </a>
-    </div>
+    
   </div>
 </nav>
+
--- includes/nav_scsScheme.php
+++ includes/nav_scsScheme.php
@@ -17,19 +17,18 @@
     </button>
 
     <div class="links-container">
-        <ul class="pages-links"> 
-            <li><a href="../index.php">MAIN PAGE</a></li>    
-            <li><a href="../index.php#portfolio">MY PORTFOLIO</a></li>
-            <!-- <li><a href="../pages/about.php">ABOUT ME</a></li> -->
-            <li><a href="../pages/codeExamples.php">CODE EXAMPLES</a></li>
-            <li><a href="../index.php#contact-me">CONTACT ME</a></li>
-        </ul>
-    </div>
-
-    <div class="nav-socials">
-      <a href="https://github.com/JoshR95" target="_blank" rel="noopener noreferrer">
-        <span class="icon-github"></span>
-      </a>
+      <ul class="pages-links"> 
+        <li><a href="../index.php">MAIN PAGE</a></li>    
+        <li><a href="../index.php#portfolio">MY PORTFOLIO</a></li>
+        <li><a href="../pages/codeExamples.php">CODE EXAMPLES</a></li>
+        <li><a href="../pages/scsScheme.php">SCS SCHEME</a></li>
+        <li><a href="../index.php#contact-me">CONTACT ME</a></li>
+      </ul>
+      <div class="nav-socials">
+        <a href="https://github.com/JoshR95">
+          <span class="icon-github"></span>
+        </a>
+      </div>
     </div>
   </div>
 </nav>
